a61c773219e3e0f11c1f1612e62ff36b1e858317
sivaranjini ganesan <sivaranjinigbtech@gmail.com>
2024-03-20 17:09:44 +0530

    CORS

--- Backend/server.py
+++ Backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask
-
+from flask_cors import CORS
 app = Flask(__name__)
-
+CORS(app)
 @app.route("/member")
 def members():
     return {"member":["Sivaranjini","Sasikannan","Sivasankare"]}
