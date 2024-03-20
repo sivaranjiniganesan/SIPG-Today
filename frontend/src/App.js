560eab26067ec636629e774fbc5edf6fef0d9a83
sivaranjini ganesan <sivaranjinigbtech@gmail.com>
2024-03-20 17:03:09 +0530

    axios changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,29 @@
 import React, { useState, useEffect} from 'react'
+import axios from 'axios'
 
 const App = () => {
   const [data, setData] = useState([{}])
+  useEffect(()=>{
+    axios.get('https://sipg-today.onrender.com/member').then(data => {
+      console.log("SUCCESS", data)
+      console.log(data)
+    }).catch(error => {
+      console.log(error)
+    })
 
-  useEffect(() => {
-    fetch("https://sipg-today.onrender.com/member").then(
-      res => res.json()
-    ).then(
-      data => {
-        setData(data)
-        console.log(data)
-      }
-    )
   }, [])
+
+  // useEffect(() => {
+    
+  //   fetch("https://sipg-today.onrender.com/member").then(
+  //     res => res.json()
+  //   ).then(
+  //     data => {
+  //       setData(data)
+  //       console.log(data)
+  //     }
+  //   )
+  // }, [])
   return (
     <div>
       {(typeof data.member === 'undefined')? (
